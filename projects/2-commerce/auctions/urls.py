9e9a06b26cd44b12d884f0933f4bc52580c17a2b
Janice <janice.vilela@gmail.com>
2025-04-22 17:00:56 +0200

    ongoing Project 2

---
+++ projects/2-commerce/auctions/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path ("create", views.createListing, name="create"),
+    path ("displayCategory", views.displayCategory, name="displayCategory"),
+    path("listing/<int:id>/", views.listing, name="listing"),
+    path("removeFromWatchlist/<int:id>/", views.removeFromWatchlist, name="removeFromWatchlist"),
+    path("addToWatchlist/<int:id>/", views.addToWatchlist, name="addToWatchlist"),
+    path("watchlist", views.displayWatchlist, name="watchlist")
+]
