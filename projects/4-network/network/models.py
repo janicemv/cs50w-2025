453558ff6b9a24477ab3c5d35bc68df7948ed86f
Janice <janice.vilela@gmail.com>
2025-05-13 11:51:43 +0200

    Project 4 ongoing - need to fix follow/unfollow

--- projects/4-network/network/models.py
+++ projects/4-network/network/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    content = models.CharField(max_length=140)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.id} made by {self.user} on {self.date.strftime('%d %b %Y %H:%M:%S')}"
+    
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followed")
+
+    def __str__(self):
+        return f"{self.follower} follows {self.followed}"
--- projects/4-network/network/views.py
+++ projects/4-network/network/views.py
@@ -3,13 +3,73 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.core.paginator import Paginator
 
-from .models import User
+from .models import User, Post, Follow
 
 
 def index(request):
-    return render(request, "network/index.html")
+    allPosts = Post.objects.all().order_by("id").reverse()
 
+    paginator = Paginator(allPosts, 10)
+    pageNumber = request.GET.get('page')
+    pagePosts = paginator.get_page(pageNumber)
+
+    return render(request, "network/index.html", {
+        "allPosts": allPosts,
+        "pagePosts": pagePosts
+    })
+
+
+def profile(request, user_id):
+    user = User.objects.get(pk=user_id)
+    userPosts = Post.objects.filter(user=user).order_by("id").reverse()
+
+    follower = Follow.objects.filter(follower=user)
+    followed = Follow.objects.filter(followed=user)
+
+    isFollowing = False
+
+    if request.user.is_authenticated:
+        isFollowing = Follow.objects.filter(follower=request.user, followed=user).exists()
+
+
+    paginator = Paginator(userPosts, 10)
+    pageNumber = request.GET.get('page')
+    pagePosts = paginator.get_page(pageNumber)
+
+    return render(request, "network/profile.html", {
+        "userPosts": userPosts,
+        "pagePosts": pagePosts,
+        "username": user.username,
+        "follower": follower,
+        "followed": followed,
+        "isFollowing": isFollowing,
+        "userProfile": user
+    })
+
+# PAREI AQUI - CONSERTAR FOLLOW E UNFOLLOW
+def follow(request):
+    userToFollowInfo = request.POST['userToFollow']
+    print("userToFollowInfo:", userToFollowInfo)
+    loggedUser = User.objects.get(pk=request.user.id)
+    userToFollow = User.objects.get(username=userToFollowInfo)
+    f = Follow(follower=loggedUser, followed=userToFollow)
+    f.save()
+    user_id = userToFollow.id
+    
+    return HttpResponseRedirect(reverse(profile, kwargs={'user_id': user_id}))
+
+
+def unfollow(request):
+    userToFollowInfo = request.POST['userToFollow']
+    loggedUser = User.objects.get(pk=request.user.id)
+    userToFollow = User.objects.get(username=userToFollowInfo)
+    f = Follow.objects.get(follower=loggedUser, followed=userToFollow)
+    f.delete()
+    user_id = userToFollow.id
+    
+    return HttpResponseRedirect(reverse(profile, kwargs={'user_id': user_id}))
 
 def login_view(request):
     if request.method == "POST":
@@ -61,3 +121,12 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+def newPost(request):
+    if request.method =="POST":
+        content = request.POST['content']
+        user = User.objects.get(pk=request.user.id)
+        post = Post(content=content, user=user)
+        post.save()
+
+        return HttpResponseRedirect(reverse("index"))
